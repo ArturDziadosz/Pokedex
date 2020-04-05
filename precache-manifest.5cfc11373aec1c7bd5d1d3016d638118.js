bc495a02724b681e73c02583a1a7b6ca03c7e4df
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-05 19:08:09 +0200

    Updates

---
+++ precache-manifest.5cfc11373aec1c7bd5d1d3016d638118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7d952cf17e8d76b7fcae063786f0e7",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "c49220095ecd0c3c891b",
+    "url": "/Pokedex/static/css/main.cebe605b.chunk.css"
+  },
+  {
+    "revision": "8e951ab7b3088c9f83ed",
+    "url": "/Pokedex/static/js/2.d95da93f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.d95da93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49220095ecd0c3c891b",
+    "url": "/Pokedex/static/js/main.bc0855fd.chunk.js"
+  },
+  {
+    "revision": "35bf2a2708c55ae54635",
+    "url": "/Pokedex/static/js/runtime-main.d24fb926.js"
+  }
+]);
