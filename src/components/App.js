0c365e775f20b6577382e0a0f82ce7909c98758a
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-09 20:53:10 +0200

    0.0.3 Initial Commit v3 - added material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
 
   render() {
     return (
-      <div className={"container"}>
-        <div className="row">
-          Pokedex
+      <>
+        <div className={"container"}>
+          <div className="row">
+            Pokedex
+          </div>
+          <span className="material-icons" style={{color: "red"}}>
+        accessibility
+        </span>
+
+
         </div>
-      </div>
+        <Button variant="contained" color="primary" startIcon={<span className="material-icons">search</span>}>
+          Search for Pokemon
+        </Button>
+      </>
     );
   }
 }
