dbf036bd37ea7c6338caf4f35a84414af2e02e68
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-10 20:15:11 +0200

    0.0.8 Added fetch function

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,40 @@ import Form from "./Form";
 import Pokemon from "./Pokemon";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      api: "https://pokeapi.co/api/v2/",
+      data: false
+    }
+  }
+
+  fetchData = (inputValue) => {
+    this.setState({
+      inputValue
+    }, () => {
+      fetch(`${this.state.api}pokemon/${this.state.inputValue}`).then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error("Connection problem");
+        }
+      }).then(data => {
+        this.setState({
+          data
+        });
+      }).catch(err => console.log(err));
+    });
+  };
 
   render() {
     return (
       <>
         <h1>Pokédex</h1>
-        <Form />
+        <Form handleAtParent={this.fetchData}/>
         <main>
-          <Pokemon />
+          <Pokemon pokemonData={this.state.data}/>
         </main>
       </>
     );
