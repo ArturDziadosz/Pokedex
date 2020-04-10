bccef8883e1a268babd9175e2b5758d152d724f7
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-10 20:18:04 +0200

    0.0.10 Added visualization of fetched data

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,22 +2,39 @@ import React, {Component} from 'react';
 import './Pokemon.scss';
 
 class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemonData: this.props.pokemonData
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.pokemonData !== this.props.pokemonData) {
+      this.setState({
+        pokemonData: this.props.pokemonData
+      })
+    }
+  }
+
 
   render() {
+    const {pokemonData} = this.state;
+
     return (
       <>
         <section className="container">
           <div className="row">
             <div className="col-11 pokemon">
-              <div className="pokemon__img" />
+              <div className="pokemon__img"/>
               <div className="pokemon__description">
                 <p>#001</p>
-                <p>Bulbasaur</p>
+                <p>{!pokemonData ? null : pokemonData.name}</p>
                 <p>Grass</p>
               </div>
             </div>
             <div className="col-11 pokemon">
-              <div className="pokemon__img" />
+              <div className="pokemon__img"/>
               <div className="pokemon__description">
                 <p>#002</p>
                 <p>Ivysaur</p>
