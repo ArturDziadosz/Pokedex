cba08ed93770dcced00bfbbfd828e49954f4e201
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-11 14:59:44 +0200

    0.0.11 Added Error.js and Error.scss

---
+++ src/components/Error.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './Error.scss';
+
+class Error extends Component {
+
+  render() {
+    return (
+      <>
+          <section className="container">
+            <div className="row">
+              <div className="col-11 error">
+                <h3>No Pokémon Matched Your Search!</h3>
+                <p>Look out for typos.</p>
+                <p>Maybe use Pokédex number or advance search.</p>
+              </div>
+            </div>
+          </section>
+      </>
+    );
+  }
+}
+
+export default Error;
