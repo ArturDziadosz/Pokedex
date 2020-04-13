886ce7483b4719778f16719d8b97dc1d451cc9d9
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-13 15:31:11 +0200

    0.1.10 Added More.js and More.scss (adding new Pokemon to list)

---
+++ src/components/More.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './More.scss';
+
+import {Button} from "@material-ui/core";
+
+class More extends Component {
+
+  handleUploadNewPokemon = e => {
+    this.props.handleAtParent(this.props.nextUrl)
+  };
+
+  render() {
+
+    return (
+      <>
+        <section className="container">
+          <div className="row row--more">
+            <Button className={"moreBtn"} onClick={this.handleUploadNewPokemon}>
+                <span className="material-icons">
+                  expand_more
+                </span>
+            </Button>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
+
+export default More;
