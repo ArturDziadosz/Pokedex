664c8ad12e6131ad64d2a1fdd0490a41ad0b7647
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-13 15:33:26 +0200

    0.1.12 Added logic for More.js and Filter.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import Form from "./Form";
 import Pokemon from "./Pokemon";
 import Error from "./Error";
 import PokemonDetails from "./PokemonDetails";
+import Filter from "./Filter";
+import More from "./More"
+
+import {Button} from "@material-ui/core";
 
 class App extends Component {
   constructor(props) {
@@ -16,22 +20,32 @@ class App extends Component {
       noMatchFound: false,
       boxWithDetails: false,
       pokemonDetails: "",
-      evolutionChainDetails: ""
+      evolutionChainDetails: "",
+      nextPokemon: "",
+      filter: {
+        azFilter: false,
+        zaFilter: false,
+        ascentFilter: true,
+        descentFilter: false
+      }
     }
   }
 
   componentDidMount() {
-    this.fetchAllPokemon(20, 0);
+    this.fetchAllPokemon(`${this.state.api}pokemon/`);
   }
 
-  fetchAllPokemon = (limit, offset) => {
-    fetch(`${this.state.api}pokemon/?offset=${offset}&limit=${limit}`).then(resp => {
+  fetchAllPokemon = (url) => {
+    fetch(url).then(resp => {
       if (resp.ok) {
         return resp.json();
       } else {
         throw new Error("Connection problem (primal fetch)");
       }
     }).then(allPokemon => {
+      this.setState({
+        nextPokemon: allPokemon.next
+      });
       allPokemon.results.forEach(pokemon => {
         this.fetchAllPokemonData(pokemon)
       });
@@ -125,47 +139,153 @@ class App extends Component {
   closeWindow = () => {
     this.setState({
       boxWithDetails: false,
-      data: false
+      data: false,
+      noMatchFound: false
     })
   };
 
+  changeFilter = id => {
+    switch (id) {
+      case 'az':
+        this.setState({
+          filter: {
+            azFilter: true,
+            zaFilter: false,
+            ascentFilter: false,
+            descentFilter: false
+          }
+        });
+        break;
+      case 'za':
+        this.setState({
+          filter: {
+            azFilter: false,
+            zaFilter: true,
+            ascentFilter: false,
+            descentFilter: false
+          }
+        });
+        break;
+      case '91':
+        this.setState({
+          filter: {
+            azFilter: false,
+            zaFilter: false,
+            ascentFilter: false,
+            descentFilter: true
+          }
+        });
+        break;
+      default:
+        this.setState({
+          filter: {
+            azFilter: false,
+            zaFilter: false,
+            ascentFilter: true,
+            descentFilter: false
+          }
+        });
+    }
+  };
+
   render() {
-    this.state.pokemonData.sort((a,b) => {
-      return a.id - b.id;
-    });
+    const filter = this.state.filter;
+
+    if (filter.ascentFilter) {
+      this.state.pokemonData.sort((a, b) => {
+        return a.id - b.id;
+      });
+    }
+
+    if (filter.descentFilter) {
+      this.state.pokemonData.sort((a, b) => {
+        return b.id - a.id;
+      });
+    }
+
+    if (filter.azFilter) {
+      this.state.pokemonData.sort((a, b) => {
+        const nameA = a.name;
+        const nameB = b.name;
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+
+    if (filter.zaFilter) {
+      this.state.pokemonData.sort((a, b) => {
+        const nameA = a.name;
+        const nameB = b.name;
+        if (nameA > nameB) {
+          return -1;
+        }
+        if (nameA < nameB) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+
 
     let main;
     if (!this.state.data && !this.state.boxWithDetails) {
-      main = <main>
-        <section className="container">
-          <div className="row">
-            <div className="col-11 start">
-              <h2>Search for a Pokémon by name or using its National Pokédex number (1-943).</h2>
-              <p>Have fun!</p>
+      main =
+        <main>
+          {this.state.noMatchFound ?
+            <Error handleAtParent={this.closeWindow}/> :
+            null
+          }
+          <section className="container">
+            <div className="row">
+              <div className="col-11 start">
+                <h2>Search for a Pokémon by name or using its National Pokédex number (1-802).</h2>
+                <p>Have fun!</p>
+              </div>
             </div>
-          </div>
-        </section>
-        {this.state.noMatchFound ? <Error/> :
-          this.state.pokemonData.map(pokemon => {
-            return <Pokemon pokemonData={pokemon} handleAtParent={this.getEvolutionUrl} key={pokemon.id}
-                            id={pokemon.id}/>
-          })}
-      </main>;
+          </section>
+          {this.state.noMatchFound ? null :
+            <>
+              <Filter activeFilter={this.state.filter} handleAtParent={this.changeFilter}/>
+              {this.state.pokemonData.map(pokemon => {
+                return <Pokemon pokemonData={pokemon} handleAtParent={this.getEvolutionUrl} key={pokemon.id}
+                                id={pokemon.id}/>
+              })}
+              <More nextUrl={this.state.nextPokemon} handleAtParent={this.fetchAllPokemon}/>
+            </>
+          }
+        </main>;
     }
 
     if (this.state.data && !this.state.boxWithDetails) {
       main = <main>
-        {this.state.noMatchFound ? <Error/> :
+        {this.state.noMatchFound ?
+          <>
+            <Error handleAtParent={this.closeWindow}/>
+            <section className="container">
+              <div className="row">
+                <div className="col-11 start">
+                  <h2>Search for a Pokémon by name or using its National Pokédex number (1-802).</h2>
+                  <p>Have fun!</p>
+                </div>
+              </div>
+            </section>
+          </> :
           <Pokemon pokemonData={this.state.data} handleAtParent={this.getEvolutionUrl} id={this.state.data.id}/>
         }
       </main>;
     }
 
     if (this.state.data && this.state.boxWithDetails) {
-      main = <main>
-        <PokemonDetails pokemonData={this.state.data} pokemonDetails={this.state.pokemonDetails}
+      main =
+        <main>
+          <PokemonDetails pokemonData={this.state.data} pokemonDetails={this.state.pokemonDetails}
                         pokemonEvolution={this.state.evolutionChainDetails} handleAtParent={this.closeWindow}/>
-      </main>;
+        </main>;
     }
 
     return (
