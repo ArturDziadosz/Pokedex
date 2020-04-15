fe5a0d7b0591b3b42ffb6d7071e303f339250312
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-04-15 22:12:36 +0200

    1.1.4 Added alt error for PokemonDetails.js problems, updating props, checking if props is object

--- src/components/Error.js
+++ src/components/Error.js
@@ -12,10 +12,14 @@ class Error extends Component {
       <>
         <section className="container">
           <div className="row row--error">
-            <div className="col-11 error">
-              <h3>No Pokémon Matched Your Search!</h3>
-              <p>Look out for typos.</p>
-              <p>Maybe use Pokédex number.</p>
+            <div className={this.props.errorText ? "col-11 error error--alt" : "col-11 error"}>
+              <h3>{this.props.errorText ? this.props.errorText : "No Pokémon Matched Your Search!"}</h3>
+              {this.props.errorText ? null :
+                <>
+                  <p>Look out for typos.</p>
+                  <p>Maybe use Pokédex number.</p>
+                </>
+              }
               <div className="material-icons" onClick={e => this.handleClose(e)}>
                 keyboard_return
               </div>
--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './PokemonDetails.scss';
 
+import Error from "./Error";
+
 class PokemonDetails extends Component {
   constructor(props) {
     super(props);
@@ -11,87 +13,111 @@ class PokemonDetails extends Component {
     }
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.pokemonData !== this.props.pokemonData) {
+      this.setState({
+        pokemonData: this.props.pokemonData,
+      })
+    }
+    if (prevProps.pokemonDetails !== this.props.pokemonDetails) {
+      this.setState({
+        pokemonDetails: this.props.pokemonDetails,
+      })
+    }
+    if (prevProps.pokemonEvolution !== this.props.pokemonEvolution) {
+      this.setState({
+        pokemonEvolution: this.props.pokemonEvolution,
+      })
+    }
+  }
+
   handleClose = () => {
     this.props.handleAtParent()
   };
 
   render() {
     const {pokemonData, pokemonDetails, pokemonEvolution} = this.state;
-    const flavorText = pokemonDetails.flavor_text_entries.filter(elem => elem.language.name === "en");
+    let flavorText;
+    if (typeof pokemonDetails === "object") {
+      flavorText = pokemonDetails.flavor_text_entries.filter(elem => elem.language.name === "en");
+    }
 
     return (
       <>
-        <section className="container">
-          <div className="row row--detailedPokemon">
-            <div className="col-11 pokemon--details">
-              <p className="material-icons pokemon__exit" onClick={e => this.handleClose(e)}>
-                keyboard_return
-              </p>
-              <p className={"pokemon__title"}><span
-                className={"pokemon__title__name"}>{pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1)} </span>
-                <span className={"pokemon__title__id"}>#{pokemonDetails.id}</span></p>
-              <div className={"pokemon__img"}
-                   style={{backgroundImage: `url(${pokemonData.sprites.front_default})`}}/>
-              <div className={"pokemon__types"}>{pokemonData.types.map(type => {
-                return <span
-                  key={type.type.name}
-                  className={"pokemon__types__type"}>{type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)} </span>;
-              })}</div>
-              <p className={"pokemon__flavorText"}>{flavorText[0].flavor_text}</p>
-              <div className={"pokemon__box"}>
-                <div className={"pokemon__box__abilities"}>Abilities:
-                  {pokemonData.abilities.map(ability => {
-                    return <p
-                      key={ability.ability.name}
-                      className={"box__abilities__ability"}>{ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)}</p>;
-                  })}
-                  <br/>
-                  <p className={"pokemon__box__height"}>Height: {pokemonData.height}</p>
-                  <p className={"pokemon__box__weight"}>Weight: {pokemonData.weight}</p>
+        {(typeof pokemonData === "string" || typeof pokemonDetails === "string") ?
+          <Error errorText={"Oops! Something goes wrong."} handleAtParent={e => this.handleClose(e)}/> :
+          <section className="container">
+            <div className="row row--detailedPokemon">
+              <div className="col-11 pokemon--details">
+                <p className="material-icons pokemon__exit" onClick={e => this.handleClose(e)}>
+                  keyboard_return
+                </p>
+                <p className={"pokemon__title"}><span
+                  className={"pokemon__title__name"}>{pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1)} </span>
+                  <span className={"pokemon__title__id"}>#{pokemonDetails.id}</span></p>
+                <div className={"pokemon__img"}
+                     style={{backgroundImage: `url(${pokemonData.sprites.front_default})`}}/>
+                <div className={"pokemon__types"}>{pokemonData.types.map(type => {
+                  return <span
+                    key={type.type.name}
+                    className={"pokemon__types__type"}>{type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)} </span>;
+                })}</div>
+                <p className={"pokemon__flavorText"}>{flavorText[0].flavor_text}</p>
+                <div className={"pokemon__box"}>
+                  <div className={"pokemon__box__abilities"}>Abilities:
+                    {pokemonData.abilities.map(ability => {
+                      return <p
+                        key={ability.ability.name}
+                        className={"box__abilities__ability"}>{ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)}</p>;
+                    })}
+                    <br/>
+                    <p className={"pokemon__box__height"}>Height: {pokemonData.height}</p>
+                    <p className={"pokemon__box__weight"}>Weight: {pokemonData.weight}</p>
+                  </div>
+                  <div className={"pokemon__box__stats"}>Statistics:
+                    {pokemonData.stats.map(stat => {
+                      return <p
+                        key={stat.stat.name}
+                        className={"box__stats__stat"}>{stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: {stat.base_stat}</p>;
+                    })}
+                  </div>
                 </div>
-                <div className={"pokemon__box__stats"}>Statistics:
-                  {pokemonData.stats.map(stat => {
-                    return <p
-                      key={stat.stat.name}
-                      className={"box__stats__stat"}>{stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: {stat.base_stat}</p>;
-                  })}
-                </div>
-              </div>
-              <div className={"pokemon__box2"}>
-                <p>Evolutions:</p>
-                {!pokemonEvolution ? null :
-                  <>
-                    <p
-                      className={pokemonDetails.name === pokemonEvolution.chain.species.name ? "active" : ""}>{pokemonEvolution.chain.species.name.charAt(0).toUpperCase() +
-                    pokemonEvolution.chain.species.name.slice(1)}</p>
-                    <p className="material-icons arrow">
-                      navigation
-                    </p>
-                  </>
-                }
-                {!pokemonEvolution ? null :
-                  pokemonEvolution.chain.evolves_to.length !== 0 ?
-                    pokemonEvolution.chain.evolves_to[0].evolves_to.length !== 0 ?
-                      <>
+                <div className={"pokemon__box2"}>
+                  <p>Evolutions:</p>
+                  {!pokemonEvolution ? null :
+                    <>
+                      <p
+                        className={pokemonDetails.name === pokemonEvolution.chain.species.name ? "active" : ""}>{pokemonEvolution.chain.species.name.charAt(0).toUpperCase() +
+                      pokemonEvolution.chain.species.name.slice(1)}</p>
+                      <p className="material-icons arrow">
+                        navigation
+                      </p>
+                    </>
+                  }
+                  {!pokemonEvolution ? null :
+                    pokemonEvolution.chain.evolves_to.length !== 0 ?
+                      pokemonEvolution.chain.evolves_to[0].evolves_to.length !== 0 ?
+                        <>
+                          <p
+                            className={pokemonDetails.name === pokemonEvolution.chain.evolves_to[0].species.name ? "active" : ""}>{pokemonEvolution.chain.evolves_to[0].species.name.charAt(0).toUpperCase() + pokemonEvolution.chain.evolves_to[0].species.name.slice(1)}
+                          </p>
+                          <p className="material-icons arrow">
+                            navigation
+                          </p>
+                          <p
+                            className={pokemonDetails.name === pokemonEvolution.chain.evolves_to[0].evolves_to[0].species.name ? "active" : ""}>{pokemonEvolution.chain.evolves_to[0].evolves_to[0].species.name.charAt(0).toUpperCase() + pokemonEvolution.chain.evolves_to[0].evolves_to[0].species.name.slice(1)}
+                          </p>
+                        </> :
                         <p
                           className={pokemonDetails.name === pokemonEvolution.chain.evolves_to[0].species.name ? "active" : ""}>{pokemonEvolution.chain.evolves_to[0].species.name.charAt(0).toUpperCase() + pokemonEvolution.chain.evolves_to[0].species.name.slice(1)}
                         </p>
-                        <p className="material-icons arrow">
-                          navigation
-                        </p>
-                        <p
-                          className={pokemonDetails.name === pokemonEvolution.chain.evolves_to[0].evolves_to[0].species.name ? "active" : ""}>{pokemonEvolution.chain.evolves_to[0].evolves_to[0].species.name.charAt(0).toUpperCase() + pokemonEvolution.chain.evolves_to[0].evolves_to[0].species.name.slice(1)}
-                        </p>
-                      </> :
-                      <p
-                        className={pokemonDetails.name === pokemonEvolution.chain.evolves_to[0].species.name ? "active" : ""}>{pokemonEvolution.chain.evolves_to[0].species.name.charAt(0).toUpperCase() + pokemonEvolution.chain.evolves_to[0].species.name.slice(1)}
-                      </p>
-                    : <p>This Pokémon does not evolve.</p>
-                }
+                      : <p>This Pokémon does not evolve.</p>
+                  }
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        }
       </>
     );
   }
